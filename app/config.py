3a5b9aa677ac0666d5bd551f5108e341557892b6
Patrick Stein <pat@nklein.com>
2024-03-11 12:09:07 -0500

    Added configuration files

---
+++ app/config.py
@@ -0,0 +1,49 @@
+import json
+
+class Config:
+    SIM_MODE = "sim_mode"
+
+    DEFAULTS = "defaults"
+    PEOPLE = "people"
+    NAME = "name"
+    LEDBUTTON = "ledbutton"
+    LED_PIN = "led_pin"
+    BUTTON_PIN = "button_pin"
+
+    SCHEDULE = "schedule"
+    MONDAY = "monday"
+    TUESDAY = "tuesday"
+    WEDNESDAY = "wednesday"
+    THURSDAY = "thursday"
+    FRIDAY = "friday"
+    SATURDAY = "saturday"
+    SUNDAY = "sunday"
+
+    def __init__(self):
+        self.config = dict()
+
+    def readConfig(self, config = "config.json"):
+        with open(config, "r") as file:
+            self.config = json.load(file)
+            print("Read configuration file")
+
+    def isSimMode(self):
+        return self.config[Config.SIM_MODE]
+
+    def getHandles(self):
+        return list(self.config[Config.PEOPLE].keys())
+
+    def getPersonName(self, handle):
+        return self.config[Config.PEOPLE][handle][Config.NAME]
+
+    def getPersonLEDPin(self, handle):
+        return self.config[Config.PEOPLE][handle][Config.LEDBUTTON][Config.LED_PIN]
+
+    def getPersonButtonPin(self, handle):
+        return self.config[Config.PEOPLE][handle][Config.LEDBUTTON][Config.BUTTON_PIN]
+
+    def getPersonSchedule(self, handle, day):
+        try:
+            return self.config[Config.PEOPLE][handle][Config.SCHEDULE][day]
+        except KeyError:
+            return self.config[Config.DEFAULTS][Config.SCHEDULE][day]
--- app/main.py
+++ app/main.py
@@ -1,16 +1,45 @@
+from config import Config
+
 import test.buttons as btest
 
 from semma_qt.factory import LEDButtonFactory
 from sim.factory import LEDButtonFactory as SimLEDButtonFactory
 
-def main():
-    print("Making button factory")
-    if False:
-        factory = LEDButtonFactory()
-    else:
-        factory = SimLEDButtonFactory()
-    print("Trying buttons")
-    btest.tryButtons(factory)
+class Application:
+    def printConfig(self):
+        for handle in self.config.getHandles():
+            print("===============================")
+            print("    handle: %s" % (handle))
+            print("      name: %s" % (self.config.getPersonName(handle)))
+            print("   led_pin: %s" % (self.config.getPersonLEDPin(handle)))
+            print("button_pin: %s" % (self.config.getPersonButtonPin(handle)))
+            print("    sunday: %s" % (self.config.getPersonSchedule(handle, Config.SUNDAY)))
+            print("    monday: %s" % (self.config.getPersonSchedule(handle, Config.MONDAY)))
+            print("   tuesday: %s" % (self.config.getPersonSchedule(handle, Config.TUESDAY)))
+            print(" wednesday: %s" % (self.config.getPersonSchedule(handle, Config.WEDNESDAY)))
+            print("  thursday: %s" % (self.config.getPersonSchedule(handle, Config.THURSDAY)))
+            print("    friday: %s" % (self.config.getPersonSchedule(handle, Config.FRIDAY)))
+            print("  saturday: %s" % (self.config.getPersonSchedule(handle, Config.SATURDAY)))
+            print("===============================")
+
+    def testButtons(self):
+        print("Making button factory")
+        if self.config.isSimMode():
+            print("Simulation mode")
+            factory = SimLEDButtonFactory()
+        else:
+            print("Real mode")
+            factory = LEDButtonFactory()
+            print("Trying buttons")
+        btest.tryButtons(factory)
+
+    def main(self):
+        print("Reading config file")
+        self.config = Config()
+        self.config.readConfig()
+        self.printConfig()
+        self.testButtons()
 
 if __name__ == "__main__":
-    main()
+    app = Application()
+    app.main()
